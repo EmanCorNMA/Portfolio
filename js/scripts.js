54501d691e342d7e4a8142d9d2516cdbeed23b9f
EmanCor <correa23@hawaii.edu>
2024-09-30 11:09:16 -1000

    added buttons for image toggle and email/phone# markup

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,50 @@
+//MONTECARLO MONACO TOURISM LOGO
 document.addEventListener('DOMContentLoaded', function () {
 
-    //JAVASCRIPT FUNCTIONS GO HERE
+    const montecarloImg = document.getElementById("montecarloImg");
+    const montecarloButton = document.getElementById("montecarloButton");
 
-    //IMAGE SWAP
-
-    function refreshSwitch(s) {
-        let currentSwitch = s.dataset.switchCurrent;
-
-        let switchAttrStr = "switchSrc-" + currentSwitch;
-        let currentSwitchSrc = s.dataset[switchAttrStr];
-
-        s.setAttribute("src", currentSwitchSrc);
-    }
-
-    function switchNext(s) {
-        let currentSwitch = parseInt(s.dataset.switchCurrent, 10);
-        let switchCases = parseInt(s.dataset.switchCases, 10);
-
-        currentSwitch += 1;
-        if (currentSwitch >= switchCases) {
-            currentSwitch = 0;
+    montecarloButton.addEventListener("click", function () {
+        if (montecarloImg.src.endsWith("img/Monaco-Tourism-Logo-02.png")) {
+            montecarloImg.src = "img/Monaco-Tourism-Logo-01.png";
+        } else {
+            montecarloImg.src = "img/Monaco-Tourism-Logo-02.png";
         }
-
-        s.dataset.switchCurrent = currentSwitch;
-    }
-
-    function initializeSwitch() {
-        let switchables = document.getElementsByClassName("switchable");
-        for (let i = 0; i < switchables.length; i += 1) {
-            let switchable = switchables[i];
-            refreshSwitch(switchable);
-            switchable.addEventListener("click", () => {
-                switchNext(switchable);
-                refreshSwitch(switchable);
-            });
-        }
-    }
-    initializeSwitch();
-
-    //TEST IF JS IS LINKED AND ACTIVE console.log('JS IS READY');
+    });
 })
+
+//SURF N SEA LOGO
+document.addEventListener('DOMContentLoaded', function () {
+
+    const surfnseaImg = document.getElementById("surfnseaImg");
+    const surfnseaButton = document.getElementById("surfnseaButton");
+
+    surfnseaButton.addEventListener("click", function () {
+        if (surfnseaImg.src.endsWith("img/Surf-n-Sea-02.png")) {
+            surfnseaImg.src = "img/Surf-n-Sea-01.png";
+        } else {
+            surfnseaImg.src = "img/Surf-n-Sea-02.png";
+        }
+    });
+})
+
+//SURF N SEA LOGO
+document.addEventListener('DOMContentLoaded', function () {
+
+    const olympicpictogramsImg = document.getElementById("olympicpictogramsImg");
+    const olympicpictogramsButton = document.getElementById("olympicpictogramsButton");
+
+    olympicpictogramsButton.addEventListener("click", function () {
+        if (olympicpictogramsImg.src.endsWith("img/Olympic-Picto-01.png")) {
+            olympicpictogramsImg.src = "img/Olympic-Picto-02.png";
+        } else {
+            olympicpictogramsImg.src = "img/Olympic-Picto-01.png";
+        }
+    });
+})
+
+//TEST IF JS IS LINKED AND ACTIVE console.log('JS IS READY');
+
 
 //BODY NO SCROLLING WHEN POPUP OPENS
 
@@ -50,3 +55,4 @@ function disableScroll() {
 function enableScroll() {
     document.body.classList.remove("remove-scrolling");
 }
+
